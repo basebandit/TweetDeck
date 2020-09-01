a543b45e8614c440758fe7979912f9d187a51d68
capricorn <capricorn@localhost.localdomain>
2020-09-01 21:29:19 +0300

    Database connection

---
+++ database/database.go
@@ -0,0 +1,57 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+//Connects to a mongodb and returns a connection handle
+//address: localhost:27017
+func Connect(ctx context.Context, address, username, password, database string) (*mongo.Database, error) {
+	connStr := fmt.Sprintf("mongodb://%s:%s@%s/?authSource=%s&connectTimoutMS=300000", username, password, address, database)
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(connStr))
+
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		return nil, err
+	}
+
+	db := client.Database(database)
+
+	return db, nil
+}
+
+// func insertRecord(ctx context.Context, db *mongo.Database, avatars []avatar, forDate time.Time) (interface{}, error) {
+// 	r := record{
+// 		ID:        primitive.NewObjectID(),
+// 		ForDate:   forDate,
+// 		CreatedAt: time.Now(),
+// 		Avatars:   avatars,
+// 	}
+
+// 	result, err := db.Collection("records").InsertOne(ctx, r)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	return result.InsertedID, nil
+// }
