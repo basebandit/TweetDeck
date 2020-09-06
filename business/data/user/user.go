e0f0a9e3fdcf7c53f3fe19a70230b713bb912382
capricorn <capricorn@localhost.localdomain>
2020-09-07 02:20:40 +0300

    user database queries

--- business/data/user/user.go
+++ business/data/user/user.go
@@ -2,6 +2,7 @@ package user
 
 import (
 	"context"
+	"database/sql"
 	"time"
 
 	"github.com/pkg/errors"
@@ -25,7 +26,7 @@ var (
 )
 
 //Create inserts a new user into the database.
-func Create(ctx context.Context, db *sqlx.DB, nu NewUser) (User, error) {
+func Create(ctx context.Context, db *sqlx.DB, nu NewUser, now time.Time) (User, error) {
 	ctx, span := global.Tracer("avatarlysis").Start(ctx, "business.data.user.create")
 	defer span.End()
 
@@ -36,7 +37,7 @@ func Create(ctx context.Context, db *sqlx.DB, nu NewUser) (User, error) {
 			return User{}, errors.Wrap(err, "generating password hash")
 		}
 
-		u.Password = hash
+		u.PasswordHash = hash
 	}
 
 	if nu.Email != nil {
@@ -46,15 +47,145 @@ func Create(ctx context.Context, db *sqlx.DB, nu NewUser) (User, error) {
 	u.ID = uuid.New().String()
 	u.Firstname = nu.Firstname
 	u.Lastname = nu.Lastname
-	u.CreatedAt = time.Now().UTC()
-	u.UpdatedAt = time.Now().UTC()
+	u.Active = true
+	u.CreatedAt = now
+	u.UpdatedAt = now
 
 	const q = `INSERT INTO users
-	(id,firstname,lastname,email,password,created_at,updated_at)
-	VALUES ($1,$2,$3,$4,$5,$6,$7)`
+	(id,firstname,lastname,email,password_hash,active,created_at,updated_at)
+	VALUES ($1,$2,$3,$4,$5,$6,$7,$8)`
 
-	if _, err := db.ExecContext(ctx, q, u.ID, u.Firstname, u.Lastname, u.Password, u.CreatedAt, u.UpdatedAt); err != nil {
+	if _, err := db.ExecContext(ctx, q, u.ID, u.Firstname, u.Lastname, u.Email, u.PasswordHash, u.Active, u.CreatedAt, u.UpdatedAt); err != nil {
 		return User{}, errors.Wrap(err, "inserting user")
 	}
 	return u, nil
+}
+
+//Update replaces an existing user record in the database.
+func Update(ctx context.Context, db *sqlx.DB, userID string, uu UpdateUser, now time.Time) error {
+	ctx, span := global.Tracer("avatarlysis").Start(ctx, "business.data.user.update")
+	defer span.End()
+
+	if _, err := uuid.Parse(userID); err != nil {
+		return ErrInvalidID
+	}
+
+	var u User
+
+	if uu.Firstname != nil {
+		u.Firstname = *uu.Firstname
+	}
+	if uu.Lastname != nil {
+		u.Lastname = *uu.Lastname
+	}
+
+	if uu.Email != nil {
+		u.Email = *uu.Email
+	}
+
+	if uu.Password != nil {
+		pw, err := bcrypt.GenerateFromPassword([]byte(*uu.Password), bcrypt.DefaultCost)
+		if err != nil {
+			return errors.Wrap(err, "generating password hash")
+		}
+		u.PasswordHash = pw
+	}
+
+	u.UpdatedAt = now
+
+	const q = `UPDATE users SET
+	 "firstname" = $2,
+	 "lastname" = $3,
+	 "email" = $4,
+	 "password_hash" = $5,
+	 "updated_at" = $6
+	 WHERE id = $1`
+
+	if _, err := db.ExecContext(ctx, q, userID, u.Firstname, u.Lastname, u.Email, u.PasswordHash, u.UpdatedAt); err != nil {
+		return errors.Wrap(err, "updating user")
+	}
+
+	return nil
+}
+
+//Delete marks an existing user record as inactive. It thus performs a soft delete.
+func Delete(ctx context.Context, db *sqlx.DB, userID string) error {
+	ctx, span := global.Tracer("avartalysis").Start(ctx, "business.data.user.delete")
+	defer span.End()
+
+	if _, err := uuid.Parse(userID); err != nil {
+		return ErrInvalidID
+	}
+
+	u := User{
+		Active: false,
+	}
+
+	const q = `UPDATE users SET
+	"active" = $2
+	WHERE id = $1`
+
+	if _, err := db.ExecContext(ctx, q, userID, u.Active); err != nil {
+		return errors.Wrapf(err, "deleting user %s", userID)
+	}
+
+	return nil
+}
+
+//Get retrieves a list of all existing user records from the database.
+func Get(ctx context.Context, db *sqlx.DB) ([]User, error) {
+	ctx, span := global.Tracer("avartalysis").Start(ctx, "business.data.user.get")
+	defer span.End()
+
+	const q = `SELECT * FROM users WHERE active = TRUE`
+
+	users := []User{}
+	if err := db.SelectContext(ctx, &users, q); err != nil {
+		return nil, errors.Wrap(err, "selecting users")
+	}
+
+	return users, nil
+}
+
+//GetByID retrieves the specified user from the database.
+func GetByID(ctx context.Context, db *sqlx.DB, userID string) (User, error) {
+	ctx, span := global.Tracer("avartalysis").Start(ctx, "business.data.user.getbyid")
+	defer span.End()
+
+	if _, err := uuid.Parse(userID); err != nil {
+		return User{}, ErrInvalidID
+	}
+
+	const q = `SELECT * FROM users WHERE id = $1 AND active = TRUE`
+
+	var u User
+	if err := db.GetContext(ctx, &u, q, userID); err != nil {
+		if err == sql.ErrNoRows {
+			return User{}, ErrNotFound
+		}
+		return User{}, errors.Wrapf(err, "selecting user %q", userID)
+	}
+	return u, nil
+}
+
+//Authenticate finds a user by their email and verifies their password against the stored hash.On success it returns nil otherwise it returns the error.
+func Authenticate(ctx context.Context, db *sqlx.DB, email, password string) error {
+	ctx, span := global.Tracer("avartalysis").Start(ctx, "business.data.user.authenticate")
+	defer span.End()
+
+	const q = `SELECT * FROM users WHERE email = $1`
+
+	var u User
+	if err := db.GetContext(ctx, &u, q, email); err != nil {
+		if err == sql.ErrNoRows {
+			return ErrAuthenticationFailure
+		}
+		return errors.Wrapf(err, "selecting single user email %s", email)
+	}
+
+	if err := bcrypt.CompareHashAndPassword(u.PasswordHash, []byte(password)); err != nil {
+		return ErrAuthenticationFailure
+	}
+
+	return nil
 }
