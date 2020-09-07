fa911801e1e460e56d223fd37678a99d1980c1b1
capricorn <capricorn@localhost.localdomain>
2020-09-07 23:46:45 +0300

    add fields from avatar profile

--- business/data/avatar/model.go
+++ business/data/avatar/model.go
@@ -4,12 +4,20 @@ import "time"
 
 //Avatar is a twitter account profile.
 type Avatar struct {
-	ID        string    `db:"id" json:"id"`                //Unique Identifier.
-	Username  string    `db:"username" json:"username"`    //The twitter handle.
-	UserID    string    `db:"user_id" json:"user_id"`      //The user who manages/runs this twitter account.
-	Active    bool      `db:"active" json:"active"`        //Use this flag to perform soft deletes.
-	CreatedAt time.Time `db:"created_at" json:"createdAt"` //When the record was added.
-	UpdatedAt time.Time `db:"updated_at" json:"updatedAt"` //When the record was last modified.
+	ID              string    `db:"id" json:"id"`                             //Unique Identifier.
+	Username        string    `db:"username" json:"username"`                 //The twitter handle.
+	UserID          string    `db:"user_id" json:"user_id"`                   //The user who manages/runs this twitter account.
+	Active          bool      `db:"active" json:"active"`                     //Use this flag to perform soft deletes.
+	Assigned        int       `json:"assigned"`                               //Shows whether this avatar profile is already assigned to an existing user or not. 0=not assigned 1= assigned
+	Followers       int       `db:"followers" json:"followers"`               //Total followers count on twitter
+	Following       int       `db:"following" json:"following"`               //Total following count on twitter
+	Tweets          int       `db:"tweets" json:"tweets"`                     //Total tweets count on twitter
+	Likes           int       `db:"likes" json:"likes"`                       //Total favorite count on twitter
+	JoinDate        string    `db:"join_date" json:"joinDate"`                //Day twitter account was created.
+	ProfileImageURL string    `db:"profile_image_url" json:"profileImageURL"` //Twitter profile image location
+	Bio             string    `db:"bio" json:"bio"`                           //Twitter account profile short description.
+	CreatedAt       time.Time `db:"created_at" json:"createdAt"`              //When the record was added.
+	UpdatedAt       time.Time `db:"updated_at" json:"updatedAt"`              //When the record was last modified.
 }
 
 //NewAvatar is what we require from clients when adding an Avatar.
