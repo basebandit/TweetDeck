a75602c6e97f161126c971804aeacb8feb861f33
capricorn <capricorn@localhost.localdomain>
2020-09-09 00:21:27 +0300

    name fk constraints

--- business/data/schema/schema.go
+++ business/data/schema/schema.go
@@ -4,19 +4,20 @@ import (
 	"github.com/dimiro1/darwin"
 	"github.com/jmoiron/sqlx"
 )
+
 //Migrate runs queries that update the db schema up to date with the migrations
 //defined in this package
-func Migrate(db *sqlx.DB)error{
- driver := darwin.NewGenericDriver(db.DB,darwin.PostgresDialect{})
- d := darwin.New(driver,migrations,nil)
- return d.Migrate()
+func Migrate(db *sqlx.DB) error {
+	driver := darwin.NewGenericDriver(db.DB, darwin.PostgresDialect{})
+	d := darwin.New(driver, migrations, nil)
+	return d.Migrate()
 }
 
 var migrations = []darwin.Migration{
 	{
-		Version: 1,
-		Description:"Add users",
-		Script:`
+		Version:     1,
+		Description: "Add users",
+		Script: `
 		CREATE TABLE IF NOT EXISTS users (
 			id UUID PRIMARY KEY,
 			firstname TEXT,
@@ -29,24 +30,24 @@ var migrations = []darwin.Migration{
 		);`,
 	},
 	{
-		Version: 2,
-		Description:"Add avatars",
-		Script:`
+		Version:     2,
+		Description: "Add avatars",
+		Script: `
 		CREATE TABLE IF NOT EXISTS avatars (
 			id UUID PRIMARY KEY,
 			username TEXT,
 			user_id UUID,
-			active BOOLEAN,
+			active BOOLEAN DEFAULT TRUE,
 		 created_at TIMESTAMP,
 		 updated_at TIMESTAMP,
 		
-		 FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE RESTRICT
+		 CONSTRAINT fk_users FOREIGN KEY(user_id) REFERENCES users(id)
 		);`,
 	},
 	{
-		Version:3,
+		Version:     3,
 		Description: "Add profiles",
-		Script:`
+		Script: `
 		CREATE TABLE IF NOT EXISTS profiles (
 			id UUID PRIMARY KEY,
 			avatar_id UUID,
@@ -63,7 +64,7 @@ var migrations = []darwin.Migration{
 		 created_at TIMESTAMP,
 		 updated_at TIMESTAMP,
 		
-		 FOREIGN KEY(avatar_id) REFERENCES avatars(id) ON DELETE RESTRICT
+		 CONSTRAINT fk_avatars FOREIGN KEY(avatar_id) REFERENCES avatars(id) 
 		);`,
 	},
 }
