b8303158a97e695462d66d4b4b957a592eea9bb0
capricorn <capricorn@localhost.localdomain>
2020-09-10 05:40:06 +0300

    comment out previous routes

--- api/routes.go
+++ api/routes.go
@@ -2,19 +2,20 @@ package api
 
 const (
 	avatars      = "/api/avatar/"
-	avatarPing   = "/api/avatar/ping"
 	avatarUpload = "/api/avatar/upload"
 
 	signup = "/api/signup"
 	login  = "/api/login"
+	ping   = "/api/ping"
 )
 
 func (s *Server) routes() {
 	//Auth
 	s.router.Post(signup, s.handleSignup)
-	s.router.Post(login, s.handleLogin)
+	s.router.Get(ping, s.handlePing)
+	// s.router.Post(login, s.handleLogin)
 	//Avatars
-	s.router.Get(avatarPing, s.handleAvatarPing)
-	s.router.Post(avatarUpload, s.handleAvatarUpload)
-	s.router.Get(avatars, s.handleGetAvatar)
+	// s.router.Get(avatarPing, s.handleAvatarPing)
+	// s.router.Post(avatarUpload, s.handleAvatarUpload)
+	// s.router.Get(avatars, s.handleGetAvatar)
 }
