d57059d8b541e5e701f38320e8c8a1c24fafea6b
capricorn <capricorn@localhost.localdomain>
2020-09-14 13:31:04 +0300

    Axios constructor

---
+++ web/src/services/Api.js
@@ -0,0 +1,21 @@
+import axios from "axios"
+
+export default (token)=>{
+  if (token){
+    const request = axios.create({
+      baseURL:`http://localhost:8880`,
+      headers:{
+        Authorization:"Bearer "+token,
+        "Content-Type":"application/json",
+      },
+      timeout:60000,
+    })
+
+    return request
+  }
+
+  return axios.create({
+    baseURL:`http://localhost:8880`,
+    timeout:60000,
+  })
+}
