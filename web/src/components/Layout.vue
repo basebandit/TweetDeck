01991d69106d3f6d71ea982b402969336d635fb1
capricorn <capricorn@localhost.localdomain>
2020-09-14 13:29:45 +0300

    Root parent component for all other components

---
+++ web/src/components/Layout.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app class="grey lighten-4">
+    <Navbar />
+    <v-main class="mx-4 mb-4">
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Navbar from "@/components/NavBar";
+
+export default {
+  name: "App",
+
+  components: {
+    Navbar,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
