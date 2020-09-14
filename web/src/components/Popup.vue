8d3f114b77f409effff980760b44deff17cd9007
capricorn <capricorn@localhost.localdomain>
2020-09-14 17:51:17 +0300

    upload and download csv file

--- web/src/components/Popup.vue
+++ web/src/components/Popup.vue
@@ -1,6 +1,7 @@
 <template>
   <v-row justify="center">
-    <v-dialog max-width="600px">
+    <v-dialog max-width="600px" v-model="dialog">
+      <Notification :message="snackbarMessage" :snackbar="snackbar" :type="snackbarType" />
       <template v-slot:activator="{on,attrs}">
         <v-btn color="success" v-on="on" v-bind="attrs">
           <v-icon left>mdi-plus</v-icon>Add new avatar
@@ -14,11 +15,11 @@
           <v-form class="px-3">
             <!-- <v-text-field v-model="handle" label="Twitter handle" prepend-icon="mdi-account-circle"></v-text-field> -->
             <!-- <v-textarea v-model="bio" label="Twitter bio" prepend-icon="mdi-clipboard-account"></v-textarea> -->
-            <v-file-input accept="text/csv" label="File input"></v-file-input>
-            <v-btn class="secondary ma-2" @click="submit">
+            <v-file-input v-model="file" accept="text/csv" label="File input"></v-file-input>
+            <v-btn class="secondary ma-2" @click="download">
               <v-icon>mdi-file-excel</v-icon>Download CSV Template
             </v-btn>
-            <v-btn class="success ma-2" @click="submit">
+            <v-btn class="success ma-2" @click="upload">
               <v-icon>mdi-file-upload</v-icon>Upload
             </v-btn>
           </v-form>
@@ -28,17 +29,52 @@
   </v-row>
 </template>
 <script>
+import AvatarService from "@/services/AvatarService";
 export default {
   name: "Popup",
   data() {
     return {
       handle: "",
+      file: "",
+      dialog: false,
+      snackbarType: "",
+      snackbarMessage: "",
+      snackbar: false,
       // bio: "",
     };
   },
   methods: {
-    submit() {
-      console.log(this.handle, this.bio);
+    async upload() {
+      let formData = new FormData();
+      formData.append("avatars", this.file);
+      try {
+        const token = window.localStorage.getItem("user");
+        const response = await AvatarService.uploadAvatars(token, formData);
+        this.snackbarType = response.data.type;
+        this.snackbarmessage = "Uploaded avatars successfully";
+        this.snackbar = true;
+        setTimeout(() => {
+          this.dialog = false;
+        }, 1000);
+        /**eslint-disable */
+        console.log(response.data.status, response.data);
+      } catch (err) {
+        setTimeout(() => {
+          this.dialog = false;
+        }, 500);
+        /**eslint-disable */
+        console.log(err.response.data.status, err.response.data);
+      }
+    },
+    download() {
+      let csvContent = "usernames\n";
+      csvContent += "username1\n";
+      let anchor = document.createElement("a");
+      anchor.href =
+        "data:text/csv;charset=utf-8," + encodeURIComponent(csvContent);
+      anchor.target = "_blank";
+      anchor.download = "avatars.csv";
+      anchor.click();
     },
   },
 };
