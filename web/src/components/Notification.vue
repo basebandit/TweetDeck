08ec6c356bec4ccd2f1cde6f89dbc6d8188c3320
capricorn <capricorn@localhost.localdomain>
2020-09-14 13:30:10 +0300

    Notification component for showing a snackbar alert

---
+++ web/src/components/Notification.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <v-snackbar :value="snackbar" :color="type" top>{{message}}</v-snackbar>
+  </div>
+</template>
+<script>
+export default {
+  name: "Notification",
+  props: {
+    snackbar: {
+      type: Boolean,
+      default: false,
+    },
+    message: {
+      type: String,
+      default: "Notification message",
+    },
+    type: {
+      type: String,
+      default: "success",
+    },
+  },
+};
+</script>
