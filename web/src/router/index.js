205db4880d43b3b76a3598fdc69e55ad5dfacebc
capricorn <capricorn@localhost.localdomain>
2020-09-14 13:33:14 +0300

    Refactor and add authentication guards

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,36 +1,73 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Dashboard from '../views/Dashboard.vue'
+import Dashboard from '@/views/Dashboard.vue'
 import Team from '@/views/Team.vue'
 import Member from '@/views/Member.vue'
+import Auth from '@/components/Auth.vue'
+import Layout from '@/components/Layout.vue'
+import Login from '@/views/Login.vue'
 
 Vue.use(VueRouter)
 
   const routes = [
-  {
-    path: '/',
-    name: 'dashboard',
-    component: Dashboard
-  },
-  {
-    path: '/avatars',
-    name: 'Avatars',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Avatars.vue')
-  },
-  {
-    path:'/team',
-    name:'Team',
-    component:Team
-  },
-  { 
-    path: '/team/member/:id',
-  name:'Member',
-   component: Member, 
-   props: true 
-  },
+    {
+      path: "/auth",
+      name: "Auth",
+      component: Auth,
+      children: [
+        {
+          path: "login",
+          name: "Login",
+          component: Login,
+        },
+      ],
+    },
+    {
+      path:"/",
+      component:Layout,
+      children:[
+        {
+          path:"/",
+          redirect:"/auth/login",
+        },
+        {
+          path: '/dashboard',
+          name: 'Dashboard',
+          component: Dashboard,
+          meta: {
+            requiresAuth: true,
+          },
+        },
+        {
+          path: '/avatars',
+          name: 'Avatars',
+          // route level code-splitting
+          // this generates a separate chunk (about.[hash].js) for this route
+          // which is lazy-loaded when the route is visited.
+          component: () => import(/* webpackChunkName: "about" */ '../views/Avatars.vue'),
+          meta: {
+            requiresAuth: true,
+          },
+        },
+        {
+          path:'/team',
+          name:'Team',
+          component:Team,
+          meta: {
+            requiresAuth: true,
+          },
+        },
+        { 
+          path: '/team/member/:id',
+          name:'Member',
+          component: Member, 
+          props: true ,
+          meta: {
+            requiresAuth: true,
+          },
+        },
+      ]
+    }
 ]
 
 const router = new VueRouter({
