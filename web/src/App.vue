e6a671f1137dea066c05c0d8af9e52a3743de811
capricorn <capricorn@localhost.localdomain>
2020-09-14 13:31:35 +0300

    Refactor

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,60 +1,9 @@
 <template>
-  <v-app class="grey lighten-4">
-    <!-- <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>-->
-    <Navbar />
-    <v-main class="mx-4 mb-4">
-      <router-view></router-view>
-    </v-main>
-  </v-app>
+  <router-view></router-view>
 </template>
 
 <script>
-import Navbar from "@/components/NavBar";
-
 export default {
   name: "App",
-
-  components: {
-    Navbar,
-  },
-
-  data: () => ({
-    //
-  }),
 };
 </script>
