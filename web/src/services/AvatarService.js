5fe05990bae360440ffa5cb6480c39bdb8cda1b1
capricorn <capricorn@localhost.localdomain>
2020-09-15 13:27:28 +0300

    add get avatar api call

--- web/src/services/AvatarService.js
+++ web/src/services/AvatarService.js
@@ -3,5 +3,8 @@ import Api from "@/services/Api"
 export default{
   uploadAvatars(token,avatarFile){
     return Api(token).post('/api/avatar/upload',avatarFile)
+  },
+  getAvatars(token){
+    return Api(token).get('/api/avatar/')
   }
 }
