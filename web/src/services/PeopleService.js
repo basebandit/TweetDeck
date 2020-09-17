b9a1e82a3d39eb0af1c446dac810afaf61c14d88
basebandit <evansonmwangi83@gmail.com>
2020-09-17 23:58:03 +0300

    add new member api call

--- web/src/services/PeopleService.js
+++ web/src/services/PeopleService.js
@@ -3,5 +3,8 @@ import Api from "@/services/Api"
 export default {
   getPeople(token) {
     return Api(token).get('/api/people')
+  },
+  addPerson(token, person) {
+    return Api(token).post('/api/people/new')
   }
 }
