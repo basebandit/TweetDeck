6fd1d75c54ff6e86929cb5be33e291e1213dbeec
capricorn <capricorn@localhost.localdomain>
2020-09-17 20:18:04 +0300

    add retrieve users by id route

--- api/routes.go
+++ api/routes.go
@@ -1,9 +1,10 @@
 package api
 
 const (
-	avatars      = "/api/avatar/"
-	avatarUpload = "/api/avatar/upload"
-	avatarAssign = "/api/avatar/assign"
+	avatars         = "/api/avatar/"
+	avatarUpload    = "/api/avatar/upload"
+	avatarAssign    = "/api/avatar/assign"
+	avatarsByUserID = "/api/avatar/{id}"
 
 	people = "/api/people"
 
@@ -15,9 +16,10 @@ const (
 func (s *Server) routes() {
 	logger := Logger(s.ctx, s.log)
 
+	authenticate := Authenticate(s.auth)
 	//middlewares
 	s.router.Use(logger)
-
+	s.router.Use(authenticate)
 	//Auth
 	s.router.Post(signup, s.handleSignup)
 	s.router.Get(ping, s.handlePing)
@@ -26,6 +28,7 @@ func (s *Server) routes() {
 	// s.router.Get(avatarPing, s.handleAvatarPing)
 	s.router.Post(avatarUpload, s.handleAvatarUpload)
 	s.router.Post(avatarAssign, s.handleAssignAvatars)
+	s.router.Get(avatarsByUserID, s.handleAvatarsByUserID)
 	s.router.Get(avatars, s.handleAvatars)
 	// s.router.Get(avatars, s.handleGetAvatar)
 
