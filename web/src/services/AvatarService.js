f96c5b67deb17f65a42c47d932c8d7a77a459d81
capricorn <capricorn@localhost.localdomain>
2020-09-17 16:39:46 +0300

    add new api calls

--- web/src/services/AvatarService.js
+++ web/src/services/AvatarService.js
@@ -1,10 +1,13 @@
 import Api from "@/services/Api"
 
-export default{
-  uploadAvatars(token,avatarFile){
-    return Api(token).post('/api/avatar/upload',avatarFile)
+export default {
+  uploadAvatars(token, avatarFile) {
+    return Api(token).post('/api/avatar/upload', avatarFile)
   },
-  getAvatars(token){
+  getAvatars(token) {
     return Api(token).get('/api/avatar/')
+  },
+  assignAvatars(token, payload) {
+    return Api(token).post('/api/avatar/assign', payload)
   }
 }
