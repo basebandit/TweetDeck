6886a0732334f30bb87874b4a88e65f57d673be8
capricorn <capricorn@localhost.localdomain>
2020-09-17 09:18:02 +0300

    environment variables

---
+++ env.sh
@@ -0,0 +1,15 @@
+export CONSUMER_KEY="s6xFFFD0AzvSBQAENJan8EtRU"
+export CONSUMER_SECRET="Se04GQkD77FnRrHtW8sVbsL8RKHWrSrnSAAqI5LMC8c38PcrTH"
+export ACCESS_TOKEN="971986487876108288-tNofEearDxpTYb75izz3qNyhD2SZTHb"
+export ACCESS_SECRET="CEoYw1Ud7vcVCdJ2uNjCIPiH6yX1SnCJfcLS1sxjPqfoC"
+export TOKEN_URL="https://api.twitter.com/oauth2/token"
+# Database settings
+export AVATARLYSIS_DB_USER="avatar"
+export AVATARLYSIS_DB_PASSWORD="av_s3cr37_!"
+export AVATARLYSIS_DB_NAME="avatars"
+export AVATARLYSIS_DB_HOST="localhost:5432"
+export AVATARLYSIS_DB_DISABLE_TLS=true
+#Auth settings
+export AVATARLYSIS_PRIVATE_KEY="$HOME/.avatarlysis/private.pem"
+export AVATARLYSIS_KEY_ID="54bb2165-71e1-41a6-af3e-7da4a0e1e2c1"
+export AVATARLYSIS_ALGORITHM="RS256"
