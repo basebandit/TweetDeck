f30f8f1f4593cec679011a84a8285247a7a1f384
capricorn <capricorn@localhost.localdomain>
2020-09-17 16:42:17 +0300

    add state and getter variables

--- web/src/store/avatars.module.js
+++ web/src/store/avatars.module.js
@@ -1,53 +1,84 @@
 import AvatarService from "@/services/AvatarService"
 
 export default {
-  namespaced:true,
-  state:{
-    avatars:[],
+  namespaced: true,
+  state: {
+    avatars: [],
     fetchAvatars: false,
+    assignAvatars: false,
   },
 
-  getters:{
-    avatars :(state) => state.avatars,
-    fetching :(state) => state.fetchAvatars
+  getters: {
+    avatars: (state) => state.avatars,
+    fetching: (state) => state.fetchAvatars,
+    assigning: (state) => state.assignAvatars,
   },
 
-  actions:{
-    getAvatars({commit},payload){
-      const {token} = payload
-      commit ("updateAvatarFetchStatus")
+  actions: {
+    getAvatars({ commit }, payload) {
+      const { token } = payload
+      commit("updateAvatarFetchStatus")
       AvatarService.getAvatars(token).then(response => {
-        setTimeout(()=>{
-          if (response.status === 200){
-            commit("avatarFetchSuccess",{
+        setTimeout(() => {
+          if (response.status === 200) {
+            commit("avatarFetchSuccess", {
               avatars: response.data,
             })
           }
-        },500)
+        }, 500)
       }).catch(err => {
-        commit("avatarFetchFailure",{
+        commit("avatarFetchFailure", {
           message: err.response.data.error
         })
       })
+    },
+    assignAvatars({ commit }, payload) {
+      const { token, assign } = payload
+      commit("assignAvatarStatus")
+      AvatarService.assignAvatars(token, assign).then(response => {
+        if (response.status === 200) {
+          commit("assignAvatarSuccess", {
+            message: "asigned avatars successfully"
+          })
+        }
+      }).catch(err => {
+        commit("assignAvatarFailure", { message: err.response.data.error })
+      })
+
     }
   },
 
-  mutations:{
-    updateAvatarFetchStatus(state){
+  mutations: {
+    updateAvatarFetchStatus(state) {
       state.fetchAvatars = true;
     },
-    avatarFetchSuccess(state,payload){
-      const {avatars} = payload;
+    avatarFetchSuccess(state, payload) {
+      const { avatars } = payload;
       /**eslint-disable */
       console.log(avatars)
       state.fetchAvatars = false;
       state.avatars = avatars;
     },
-    avatarFetchFailure(state,payload){
-      const{message} = payload;
+    avatarFetchFailure(state, payload) {
+      const { message } = payload;
       /**eslint-disable */
-      console.error("avatarFetchFailure",message)
+      console.error("avatarFetchFailure", message)
       state.fetchAvatars = false;
+    },
+    assignAvatarStatus(state) {
+      state.assignAvatars = true
+    },
+    assignAvatarSuccess(state, payload) {
+      const { message } = payload
+      /**eslint-disable */
+      console.log("assignAvatarsSuccess", message)
+      state.assignAvatars = false;
+    },
+    assignAvatarFailure(state, payload) {
+      const { message } = payload
+      /**eslint-disable */
+      console.log("assignAvatarFailure", message)
+      state.assignAvatars = false;
     }
   },
 };
