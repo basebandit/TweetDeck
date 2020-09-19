9d9c1bec70f7f2f4402be6dc1b6e35761ca1503a
capricorn <capricorn@localhost.localdomain>
2020-09-20 02:49:01 +0300

    Replace with real existing accounts

--- business/data/profile/profile.go
+++ business/data/profile/profile.go
@@ -54,7 +54,66 @@ func CreateMultiple(ctx context.Context, db *sqlx.DB, np []NewProfile, now time.
 
 		q += fmt.Sprintf("($%d,$%d,$%d,$%d,$%d,$%d,$%d,$%d,$%d,$%d,$%d,$%d,$%d,$%d),", p1+1, p1+2, p1+3, p1+4, p1+5, p1+6, p1+7, p1+8, p1+9, p1+10, p1+11, p1+12, p1+13, p1+14)
 
-		insertParams = append(insertParams, p.ID, *p.AvatarID, *p.Followers, *p.Following, *p.Tweets, *p.Likes, *p.Bio, *p.Name, *p.TwitterID, *p.ProfileImageURL, *p.LastTweetTime, *p.JoinDate, p.CreatedAt, p.UpdatedAt)
+		var (
+			avatarID        string
+			following       int
+			followers       int
+			tweets          int
+			likes           int
+			bio             string
+			name            string
+			twitterID       string
+			profileImageURL string
+			lastTweetTime   string
+			joinDate        string
+		)
+
+		if p.Following != nil {
+			following = *p.Following
+		}
+		if p.Followers != nil {
+			followers = *p.Following
+		}
+
+		if p.Tweets != nil {
+			tweets = *p.Tweets
+		}
+
+		if p.Bio != nil {
+			bio = *p.Bio
+		}
+
+		if p.TwitterID != nil {
+			twitterID = *p.TwitterID
+		}
+
+		if p.Likes != nil {
+			likes = *p.Likes
+		}
+
+		if p.JoinDate != nil {
+			joinDate = *p.JoinDate
+		}
+
+		if p.Name != nil {
+			name = *p.Name
+		}
+
+		if p.AvatarID != nil {
+			avatarID = *p.AvatarID
+		}
+		if p.LastTweetTime != nil {
+			lastTweetTime = *p.LastTweetTime
+		}
+
+		if p.ProfileImageURL != nil {
+			profileImageURL = *p.ProfileImageURL
+		}
+
+		p.CreatedAt = now
+		p.UpdatedAt = now
+
+		insertParams = append(insertParams, p.ID, avatarID, followers, following, tweets, likes, bio, name, twitterID, profileImageURL, lastTweetTime, joinDate, p.CreatedAt, p.UpdatedAt)
 	}
 
 	q = q[:len(q)-1] //remove trailing ","
