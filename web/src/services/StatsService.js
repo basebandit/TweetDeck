dc518fb3288b90a826c521ca698573b5455a2118
capricorn <capricorn@localhost.localdomain>
2020-10-04 12:25:42 +0300

    fetch top avatars

--- web/src/services/StatsService.js
+++ web/src/services/StatsService.js
@@ -4,4 +4,13 @@ export default{
   getTotals(token){
  return Api(token).get('/api/totals')
   },
+  getTops(token){
+    return Api(token).get('/api/totals/top')
+  },
+  // getTopByTweets(token){
+  //   return Api(token).get('/api/top/tweets')
+  // },
+  // getTopByFollowers(token){
+  //   return Api(token).get('/api/top/followers')
+  // }
 }
