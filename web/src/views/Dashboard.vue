d5427a83c91debe89a6cbf5682e187e30bc293ce
capricorn <capricorn@localhost.localdomain>
2020-10-04 20:14:39 +0300

    add accounts stats overview

--- web/src/views/Dashboard.vue
+++ web/src/views/Dashboard.vue
@@ -1,23 +1,82 @@
 <template>
   <div class="home">
-    <h1 class="subheading grey--text">Dashboard</h1>
+    <v-toolbar
+      class="pt-3"
+      extended
+      flat
+      style="position: -webkit-sticky; position: sticky; top: 4rem; z-index: 1"
+    >
+      <v-row no-gutters class="mt-3 mb-0">
+        <v-col align="left">
+          <span class="headline grey--text">Dashboard</span>
+        </v-col>
 
-    <v-container class="my-5">
-      <v-row class="mb-3">
-        <v-col v-for="(stat, index) in stats" :key="index">
-          <v-card :class="`stat ${stat.name}`">
-            <v-card-title class="headline">
-              <span>{{ stat.total }}</span>
-              <v-spacer></v-spacer>
-              <span>
-                <v-icon right color="primary">{{ stat.icon }}</v-icon>
-              </span>
-            </v-card-title>
-            <v-card-subtitle>{{ stat.name }}</v-card-subtitle>
-          </v-card>
+        <v-col align="right">
+          <v-btn
+            rounded
+            color="primary"
+            dark
+            class="text-center"
+            @click="dailyReport"
+          >
+            <v-icon left>mdi-file-pdf</v-icon> Print Report
+          </v-btn>
         </v-col>
       </v-row>
-      <!-- <v-row class="mb-3">
+    </v-toolbar>
+    <v-container class="my-3">
+      <div ref="contentHTML" id="contentHTML">
+        <v-row class="my-5">
+          <v-col justify="space-around">
+            <v-card class="pa-2">
+              <v-card-title class="subheading grey--text">
+                Accounts Overview
+              </v-card-title>
+              <v-row justify="space-around">
+                <v-btn text color="success">
+                  <v-icon dark left>mdi-account-check</v-icon>
+                  <span class="mx-2">{{ accounts.active }}</span
+                  >Active Accounts
+                </v-btn>
+                <span class="mx-1">|</span>
+                <v-btn text color="error">
+                  <v-icon>mdi-account-alert</v-icon>
+                  <span class="mx-2">{{ accounts.suspended }}</span> Suspended
+                  Accounts
+                </v-btn>
+                <span class="mx-1">|</span>
+                <v-btn text color="primary">
+                  <v-icon>mdi-account-multiple-remove</v-icon>
+                  <span class="mx-2">{{ accounts.assigned }}</span> Assigned
+                  Active Accounts
+                </v-btn>
+                <span class="mx-1">|</span>
+                <v-btn text disabled>
+                  <v-icon>mdi-account-multiple-check</v-icon>
+                  <span class="mx-2">{{ accounts.unassigned }}</span> Unassigned
+                  Active Accounts
+                </v-btn>
+              </v-row>
+            </v-card>
+          </v-col>
+        </v-row>
+
+        <v-row class="mb-3">
+          <v-col v-for="(stat, index) in stats" :key="index">
+            <v-card :class="`stat ${stat.name}`">
+              <v-card-title class="headline">
+                <span>{{ stat.total }}</span>
+                <v-spacer></v-spacer>
+                <span>
+                  <v-icon right color="primary">{{ stat.icon }}</v-icon>
+                </span>
+              </v-card-title>
+              <v-card-subtitle>{{ stat.name }}</v-card-subtitle>
+            </v-card>
+          </v-col>
+        </v-row>
+
+        <!-- <v-row class="mb-3">
         <v-col cols="12">
           <v-card>
             <v-card-title class="headline">Total Daily Tweets</v-card-title>
@@ -35,93 +94,97 @@
         </v-col>
       </v-row> -->
 
-      <v-row class="mb-3">
-        <v-col cols="12" lg="8" md="8">
-          <v-card flat class="mx-auto" v-if="tops.tweets">
-            <v-card-title class="align-start"
-              >Top 5 Daily Avatars By Tweets</v-card-title
-            >
-            <div
-              v-for="(avatar, index) in tops.tweets"
-              :key="avatar.username"
-              :class="`avatar-${index + 1}`"
-            >
-              <v-row class="px-3">
-                <v-col>
-                  <div class="caption grey--text">Person</div>
-                  <div>{{ avatar.person || "Unassigned" }}</div>
-                </v-col>
-                <v-col>
-                  <div class="caption grey--text">Avatar</div>
-                  <div>@{{ avatar.username }}</div>
-                </v-col>
-                <v-col>
-                  <div class="caption grey--text">Number of tweets</div>
-                  <div>{{ avatar.tweets }}</div>
-                </v-col>
-              </v-row>
-              <v-divider></v-divider>
-            </div>
-          </v-card>
-        </v-col>
-        <v-col cols="12" md="4" lg="4">
-          <v-card class="mx-auto pa-3" v-if="tops.tweets">
-            <v-card-title class="headline"
-              >Top 5 Daily Avatars By Tweets</v-card-title
-            >
-            <v-card-text>
-              <pie-chart :round="2" suffix="%" :data="topFiveAvatarsByTweets" />
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
+        <v-row class="mb-3">
+          <v-col cols="12" lg="8" md="8">
+            <v-card flat class="mx-auto" v-if="tops.tweets">
+              <v-card-title class="align-start"
+                >Top 5 Daily Avatars By Tweets</v-card-title
+              >
+              <div
+                v-for="(avatar, index) in tops.tweets"
+                :key="avatar.username"
+                :class="`avatar-${index + 1}`"
+              >
+                <v-row class="px-3">
+                  <v-col>
+                    <div class="caption grey--text">Person</div>
+                    <div>{{ avatar.person || "Unassigned" }}</div>
+                  </v-col>
+                  <v-col>
+                    <div class="caption grey--text">Avatar</div>
+                    <div>@{{ avatar.username }}</div>
+                  </v-col>
+                  <v-col>
+                    <div class="caption grey--text">Number of tweets</div>
+                    <div>{{ avatar.tweets }}</div>
+                  </v-col>
+                </v-row>
+                <v-divider></v-divider>
+              </div>
+            </v-card>
+          </v-col>
+          <v-col cols="12" md="4" lg="4">
+            <v-card class="mx-auto pa-3" v-if="tops.tweets">
+              <v-card-title class="headline"
+                >Top 5 Daily Avatars By Tweets</v-card-title
+              >
+              <v-card-text>
+                <pie-chart
+                  :round="2"
+                  suffix="%"
+                  :data="topFiveAvatarsByTweets"
+                />
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
 
-      <v-row class="mb-3">
-        <v-col cols="12" lg="8" md="8">
-          <v-card flat class="mx-auto" v-if="tops.following">
-            <v-card-title class="align-start"
-              >Top 5 Daily Avatars By Following</v-card-title
-            >
-            <div
-              v-for="(avatar, index) in tops.following"
-              :key="avatar.username"
-              :class="`avatar-${index + 1}`"
-            >
-              <v-row class="px-3">
-                <v-col>
-                  <div class="caption grey--text">Person</div>
-                  <div>{{ avatar.person || "Unassigned" }}</div>
-                </v-col>
-                <v-col>
-                  <div class="caption grey--text">Avatar</div>
-                  <div>@{{ avatar.username }}</div>
-                </v-col>
-                <v-col>
-                  <div class="caption grey--text">Number of followings</div>
-                  <div>{{ avatar.following }}</div>
-                </v-col>
-              </v-row>
-              <v-divider></v-divider>
-            </div>
-          </v-card>
-        </v-col>
-        <v-col cols="12" md="4" lg="4">
-          <v-card class="mx-auto pa-3" v-if="tops.following">
-            <v-card-title class="headline"
-              >Top 5 Daily Avatars By Following</v-card-title
-            >
-            <v-card-text>
-              <pie-chart
-                :round="2"
-                suffix="%"
-                :data="topFiveAvatarsByFollowing"
-              />
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
+        <v-row class="mb-3">
+          <v-col cols="12" lg="8" md="8">
+            <v-card flat class="mx-auto" v-if="tops.following">
+              <v-card-title class="align-start"
+                >Top 5 Daily Avatars By Following</v-card-title
+              >
+              <div
+                v-for="(avatar, index) in tops.following"
+                :key="avatar.username"
+                :class="`avatar-${index + 1}`"
+              >
+                <v-row class="px-3">
+                  <v-col>
+                    <div class="caption grey--text">Person</div>
+                    <div>{{ avatar.person || "Unassigned" }}</div>
+                  </v-col>
+                  <v-col>
+                    <div class="caption grey--text">Avatar</div>
+                    <div>@{{ avatar.username }}</div>
+                  </v-col>
+                  <v-col>
+                    <div class="caption grey--text">Number of followings</div>
+                    <div>{{ avatar.following }}</div>
+                  </v-col>
+                </v-row>
+                <v-divider></v-divider>
+              </div>
+            </v-card>
+          </v-col>
+          <v-col cols="12" md="4" lg="4">
+            <v-card class="mx-auto pa-3" v-if="tops.following">
+              <v-card-title class="headline"
+                >Top 5 Daily Avatars By Following</v-card-title
+              >
+              <v-card-text>
+                <pie-chart
+                  :round="2"
+                  suffix="%"
+                  :data="topFiveAvatarsByFollowing"
+                />
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
 
-      <!-- <v-row class="mb-3">
+        <!-- <v-row class="mb-3">
         <v-col cols="12">
           <v-card>
             <v-card-title class="headline">Total Daily Followers</v-card-title>
@@ -138,118 +201,103 @@
           </v-card>
         </v-col>
       </v-row> -->
-      <v-row class="mb-3">
-        <v-col cols="12" lg="8" md="8">
-          <v-card flat class="mx-auto" v-if="tops.followers">
-            <v-card-title class="align-start"
-              >Top 5 Daily Avatars By Followers</v-card-title
-            >
-            <div
-              v-for="(avatar, index) in tops.followers"
-              :key="avatar.username"
-              :class="`avatar-${index + 1}`"
-            >
-              <v-row class="px-3">
-                <v-col>
-                  <div class="caption grey--text">Person</div>
-                  <div>{{ avatar.person || "Unassigned" }}</div>
-                </v-col>
-                <v-col>
-                  <div class="caption grey--text">Avatar</div>
-                  <div>@{{ avatar.username }}</div>
-                </v-col>
-                <v-col>
-                  <div class="caption grey--text">Number of followers</div>
-                  <div>{{ avatar.followers }}</div>
-                </v-col>
-              </v-row>
-              <v-divider></v-divider>
-            </div>
-          </v-card>
-        </v-col>
-        <v-col cols="12" md="4" lg="4">
-          <v-card class="pa-3" v-if="tops.followers">
-            <v-card-title class="headline"
-              >Top 5 Daily Avatars By Followers</v-card-title
-            >
-            <v-card-text>
-              <pie-chart
-                :round="2"
-                suffix="%"
-                :data="topFiveAvatarsByFollowers"
-              />
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
+        <v-row class="mb-3">
+          <v-col cols="12" lg="8" md="8">
+            <v-card flat class="mx-auto" v-if="tops.followers">
+              <v-card-title class="align-start"
+                >Top 5 Daily Avatars By Followers</v-card-title
+              >
+              <div
+                v-for="(avatar, index) in tops.followers"
+                :key="avatar.username"
+                :class="`avatar-${index + 1}`"
+              >
+                <v-row class="px-3">
+                  <v-col>
+                    <div class="caption grey--text">Person</div>
+                    <div>{{ avatar.person || "Unassigned" }}</div>
+                  </v-col>
+                  <v-col>
+                    <div class="caption grey--text">Avatar</div>
+                    <div>@{{ avatar.username }}</div>
+                  </v-col>
+                  <v-col>
+                    <div class="caption grey--text">Number of followers</div>
+                    <div>{{ avatar.followers }}</div>
+                  </v-col>
+                </v-row>
+                <v-divider></v-divider>
+              </div>
+            </v-card>
+          </v-col>
+          <v-col cols="12" md="4" lg="4">
+            <v-card class="pa-3" v-if="tops.followers">
+              <v-card-title class="headline"
+                >Top 5 Daily Avatars By Followers</v-card-title
+              >
+              <v-card-text>
+                <pie-chart
+                  :round="2"
+                  suffix="%"
+                  :data="topFiveAvatarsByFollowers"
+                />
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </div>
     </v-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import { jsPDF } from "jspdf";
+// import html2canvas from "html2canvas";
 import { mapGetters } from "vuex";
 export default {
   name: "Home",
   data() {
-    return {
-      totalDailyTweets: {
-        Mon: 789,
-        Tue: 876,
-        Wed: 980,
-        Thur: 680,
-        Fri: 920,
-        Sat: 689,
-        Sun: 920,
-      },
-      totalDailyFollowers: {
-        Mon: 480,
-        Tue: 500,
-        Wed: 480,
-        Thur: 550,
-        Fri: 650,
-        Sat: 750,
-        Sun: 800,
-      },
-      topWeeklyAvatarFollowers: [
-        {
-          handle: "Maina Kamanda Elder",
-          followers: 400,
-          person: "Jane Wanjiku",
-        },
-        {
-          handle: "Cate Mukami",
-          followers: 365,
-          person: "Eric Kinyua",
-        },
-        {
-          handle: "Mwasya kyalo",
-          followers: 323,
-          person: "Francis Ngaruiya",
-        },
-        {
-          handle: "Harris Kimani",
-          followers: 235,
-          person: "Stephen Ndungu",
-        },
-        {
-          handle: "Salma kambo",
-          followers: 189,
-          person: "Stephen Ndungu",
-        },
-      ],
-    };
+    return {};
   },
   mounted() {
     this.$store.dispatch("people/getPeople", { token: this.token });
     this.$store.dispatch("stats/getTotals", { token: this.token });
     this.$store.dispatch("stats/getTops", { token: this.token });
+    this.$store.dispatch("avatars/getAvatars", { token: this.token });
   },
   computed: {
     ...mapGetters("people", ["team"]),
+    ...mapGetters("avatars", ["avatars"]),
     ...mapGetters("stats", ["totals", "tops"]),
     token() {
       return window.localStorage.getItem("user");
+    },
+    accounts() {
+      let suspended = 0;
+      let active = 0;
+      let assigned = 0;
+      let unassigned = 0;
+      this.avatars.forEach((avatar) => {
+        if (Object.keys(avatar).length <= 6) {
+          suspended++;
+        } else {
+          active++;
+          //we also calculate assigned and unassigned here. Since you can only assign an account if
+          //it is not already suspended.
+          if (avatar.assigned === 1) {
+            assigned++;
+          } else if (avatar.assigned === 0) {
+            unassigned++;
+          }
+        }
+      });
+
+      return {
+        suspended: suspended,
+        active: active,
+        assigned: assigned,
+        unassigned: unassigned,
+      };
     },
     stats() {
       return [
@@ -299,9 +347,28 @@ export default {
       return obj;
     },
   },
+  methods: {
+    dailyReport() {
+      let doc = new jsPDF("p", "pt", "A4");
+      let margins = {
+        top: 80,
+        bottom: 60,
+        left: 40,
+        width: 822,
+      };
+
+      doc.html(document.getElementById("contentHTML"), {
+        callback: function (pdf) {
+          pdf.save("a4.pdf");
+        },
+        x: margins.left,
+        y: margins.top,
+      });
+    },
+  },
 };
 </script>
-<style>
+<style scoped>
 .stat.avatars {
   border-left: 4px solid #3cd1c2;
 }
