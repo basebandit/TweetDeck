67f96952122168b6d51a2e264fd0c5668a014e8d
capricorn <capricorn@localhost.localdomain>
2020-10-07 14:51:39 +0300

    add table for top tweets

--- web/src/components/DailyPdf.vue
+++ web/src/components/DailyPdf.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
+  <div id="document">
     <v-container>
-      <v-row justify="center">
+      <v-row justify="center" class="font-weight-bold">
         <v-col align="left"> Cyclones </v-col>
         <v-spacer></v-spacer>
         <v-col align="right">
@@ -12,16 +12,95 @@
       <v-divider></v-divider>
       <v-row align-content="space-between">
         <v-col class="text-left">
-          <div>TOTAL TWITTER ACCOUNTS</div>
-          <div>{{ activeAccts }}</div>
+          <div class="text-decoration-underline">TOTAL TWITTER ACCOUNTS</div>
+          <div class="font-weight-bold">{{ activeAccts.toLocaleString() }}</div>
         </v-col>
         <v-col class="text-center">
-          <div>TOTAL NEW ACCOUNTS</div>
-          <div>{{ newAccts }}</div>
+          <div class="text-decoration-underline">TOTAL NEW ACCOUNTS</div>
+          <div class="font-weight-bold">{{ newAccts.toLocaleString() }}</div>
         </v-col>
         <v-col class="text-right">
-          <div>TIMEFRAME (PERIOD)</div>
-          <div>{{ date | formatDate }}</div>
+          <div class="text-decoration-underline">TIMEFRAME (PERIOD)</div>
+          <div class="font-weight-bold">{{ date | formatDate }}</div>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col class="text-left">
+          <div class="text-decoration-underline">TOTAL DAY'S FOLLOWERS</div>
+          <div class="font-weight-bold">
+            {{ totalFollowers.toLocaleString() }}
+          </div>
+        </v-col>
+        <v-col class="text-center">
+          <div class="text-decoration-underline">TOTAL DAY'S FOLLOWING</div>
+          <div class="font-weight-bold">
+            {{ totalFollowing.toLocaleString() }}
+          </div>
+        </v-col>
+        <v-col class="text-right">
+          <div class="text-decoration-underline">TOTAL DAY'S TWEETS</div>
+          <div class="font-weight-bold">{{ totalTweets.toLocaleString() }}</div>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col class="text-center">
+          <div class="text-decoration-underline">TOTAL DAY'S LIKES</div>
+          <div class="font-weight-bold">{{ totalLikes.toLocaleString() }}</div>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col class="text-center">
+          <div class="text-decoration-underline">
+            AVATAR WITH THE HIGHEST GAINED FOLLOWERS
+          </div>
+          <div class="font-weight-bold">
+            {{ highestGainedFollowers.avatar }}
+            {{ highestGainedFollowers.count.toLocaleString() }} -
+            {{ highestGainedFollowers.person }}
+          </div>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col align="left">
+          <div class="text-decoration-underline">
+            AVATAR WITH THE HIGHEST LIKES
+          </div>
+          <div class="font-weight-bold">
+            {{ highestLikes.avatar }}
+            {{ highestLikes.count.toLocaleString() }} -
+            {{ highestLikes.person }}
+          </div>
+        </v-col>
+        <v-col align="right">
+          <div class="text-decoration-underline">TOTAL UNUSED ACCOUNTS</div>
+          <div class="font-weight-bold">
+            {{ totalUnusedAccounts.toLocaleString() }}
+          </div>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <div class="text-decoration-underline">TOP 5 AVATARS (BY TWEETS)</div>
+          <template>
+            <v-simple-table>
+              <template v-slot:default>
+                <thead>
+                  <tr>
+                    <th class="text-left">REAL NAME</th>
+                    <th class="text-left">AVATAR NAME</th>
+                    <th class="text-left">NUMBER OF TWEETS</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="item in tops.tweets" :key="item.username">
+                    <td>{{ item.person }}</td>
+                    <td>{{ item.username }}</td>
+                    <td>{{ item.tweets.toLocaleString() }}</td>
+                  </tr>
+                </tbody>
+              </template>
+            </v-simple-table>
+          </template>
         </v-col>
       </v-row>
     </v-container>
@@ -45,6 +124,133 @@ export default {
     newAccts: {
       type: Number,
       default: 0,
+    },
+    totalLikes: {
+      type: Number,
+      default: 0,
+    },
+    totalFollowers: {
+      type: Number,
+      default: 0,
+    },
+    totalFollowing: {
+      type: Number,
+      default: 0,
+    },
+    totalTweets: {
+      type: Number,
+      default: 0,
+    },
+    highestGainedFollowers: {
+      type: Object,
+      default: function () {
+        return {
+          avatar: "John Doe",
+          person: "John Doe",
+          count: 0,
+        };
+      },
+    },
+    highestLikes: {
+      type: Object,
+      default: function () {
+        return {
+          avatar: "John Doe",
+          person: "John Doe",
+          count: 0,
+        };
+      },
+    },
+    totalUnusedAccounts: {
+      type: Number,
+      default: 0,
+    },
+    tops: {
+      type: Object,
+      default: function () {
+        return {
+          tweets: [
+            {
+              username: "KairituK",
+              person: "Unassigned",
+              tweets: 4395,
+            },
+            {
+              username: "ni_witu",
+              person: "Unassigned",
+              tweets: 3404,
+            },
+            {
+              username: "h_nyinyi",
+              tweets: 3158,
+              person: "Unassigned",
+            },
+            {
+              username: "MwasikaRomelu",
+              person: "Unassigned",
+              tweets: 3058,
+            },
+            {
+              username: "NaisulaB",
+              person: "Unassigned",
+              tweets: 2983,
+            },
+          ],
+          followers: [
+            {
+              username: "JanetKariuki16",
+              person: "Unassigned",
+              followers: 2739,
+            },
+            {
+              username: "KabogoSharon",
+              person: "Unassigned",
+              followers: 1947,
+            },
+            {
+              username: "CalebMutonyi1",
+              person: "Unassigned",
+              followers: 1610,
+            },
+            {
+              username: "CMoyaka",
+              person: "Unassigned",
+              followers: 1488,
+            },
+            {
+              username: "Lydia_Korir_C",
+              person: "Unassigned",
+              followers: 1309,
+            },
+          ],
+        };
+      },
+    },
+    topFiveAvatarByTweets: {
+      type: Object,
+      default: function () {
+        return {
+          JohnDoe: 0.0,
+          JohnDoey: 0.1,
+          JohnDoek: 0.2,
+        };
+      },
+    },
+    topFiveAvatarByFollowers: {
+      type: Object,
+      default: function () {
+        return {
+          JohnDoe: 0.0,
+          JohnDoey: 0.1,
+          JohnDoek: 0.2,
+        };
+      },
+    },
+    suspendedAccounts: {
+      type: Object,
+      default: function () {
+        return {};
+      },
     },
   },
 };
