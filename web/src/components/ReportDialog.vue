eac9f2756dbac909d47910a0a4f55598caf32737
capricorn <capricorn@localhost.localdomain>
2020-10-07 09:44:43 +0300

    report modal

---
+++ web/src/components/ReportDialog.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-row justify="center">
+    <v-dialog
+      v-model="showDialog"
+      fullscreen
+      hide-overlay
+      transition="dialog-bottom-transition"
+    >
+      <v-card>
+        <v-toolbar dark color="primary">
+          <v-btn icon dark @click="closeDialog">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+          <v-toolbar-title>Report</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-toolbar-items>
+            <v-btn dark text @click="closeDialog"> Save </v-btn>
+          </v-toolbar-items>
+        </v-toolbar>
+        <daily-pdf />
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+<script>
+import DailyPdf from "./DailyPdf";
+import { mapGetters } from "vuex";
+export default {
+  name: "ReportDialog",
+  components: { DailyPdf },
+  computed: {
+    ...mapGetters("report", ["showDialog"]),
+  },
+  methods: {
+    closeDialog() {
+      this.$store.dispatch("report/hideDialog");
+    },
+  },
+};
+</script>
