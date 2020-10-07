0f3da7f050836f177afed5a1cc3cd167472ae36a
capricorn <capricorn@localhost.localdomain>
2020-10-07 09:45:33 +0300

    import report module namespace

--- web/src/store/index.js
+++ web/src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue"
 import Vuex from "vuex"
 import avatars from "./avatars.module"
+import report from "./report.module"
 import people from "./people.module"
 import stats from "./stats.module"
 
@@ -9,6 +10,7 @@ Vue.use(Vuex)
 export const store = new Vuex.Store({
   modules:{
     avatars,
+    report,
     people,
     stats,
   },
