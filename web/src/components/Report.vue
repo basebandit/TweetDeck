4699c010aa5ae0693cd9598ab19efd1c650535a3
capricorn <capricorn@localhost.localdomain>
2020-10-09 18:41:56 +0300

    Parent component for report views for use in routes

---
+++ web/src/components/Report.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <!-- To allow for switching between auth views-->
+    <!-- Example Switching between login/signin and signup-->
+    <router-view></router-view>
+  </div>
+</template>
