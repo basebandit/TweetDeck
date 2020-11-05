d288c08184b04a67de7209e6fa33a7c5431d494a
basebandit <evansonmwangi83@gmail.com>
2020-11-05 14:27:00 +0300

    handle errors gracefully

--- foundation/service/twitter/service.go
+++ foundation/service/twitter/service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	"github.com/dghubble/go-twitter/twitter"
@@ -15,27 +16,31 @@ type TwitterService struct {
 	ConsumerKey    string
 	ConsumerSecret string
 	AccessToken    string
+	AccessSecret   string
 	TokenURL       string
 }
 
 //NewTwitter initializes a twitter service object and returns it.
-func NewTwitter(key, secret, token, url string) *TwitterService {
+func NewTwitter(consumerKey, consumerSecret, accessSecret, accessToken, tokenURL string) *TwitterService {
 	t := TwitterService{
-		ConsumerKey:    key,
-		ConsumerSecret: secret,
-		AccessToken:    token,
-		TokenURL:       url,
+		ConsumerKey:    consumerKey,
+		ConsumerSecret: consumerSecret,
+		AccessToken:    accessToken,
+		AccessSecret:   accessSecret,
+		TokenURL:       tokenURL,
 	}
 
 	return &t
 }
 
 func (t *TwitterService) client() *twitter.Client {
+	// //==== This is App Only Auth uses oauth 2
 	config := &clientcredentials.Config{
 		ClientID:     t.ConsumerKey,
 		ClientSecret: t.ConsumerSecret,
 		TokenURL:     t.TokenURL,
 	}
+
 	// http.Client will automatically authorize Requests
 	httpClient := config.Client(oauth2.NoContext)
 	//Twitter client
@@ -45,7 +50,7 @@ func (t *TwitterService) client() *twitter.Client {
 }
 
 //Lookup fetches the profiles of the given username(s)
-func (t *TwitterService) Lookup(ctx context.Context, usernames []string) []twitter.User {
+func (t *TwitterService) Lookup(ctx context.Context, usernames []string) ([]twitter.User, error) {
 	ctx, span := global.Tracer("avatarlysis").Start(ctx, "foundation.service.twitter.lookup")
 	defer span.End()
 
@@ -55,19 +60,27 @@ func (t *TwitterService) Lookup(ctx context.Context, usernames []string) []twitt
 		ScreenName: usernames,
 	}
 
-	fmt.Println("Twitter lookup started...")
+	fmt.Println("Twitter lookup started now...")
 
-	users, tres, err := client.Users.Lookup(userLookupParams)
-	if tres.StatusCode != 200 {
-		fmt.Printf("oops something unexpected occured: %s response code [%v]\n", err, tres.StatusCode)
+	if client != nil {
+		users, tres, err := client.Users.Lookup(userLookupParams)
+		if tres != nil {
+			if tres.StatusCode != 200 {
+				fmt.Printf("oops something unexpected occured response code: [%v]\n", tres.StatusCode)
+			}
+		}
+
+		if err != nil {
+			fmt.Printf("an error occurred looking up twitter profiles error: %s\n", err)
+			return nil, err
+		}
+
+		fmt.Println("Twitter lookup finished successfully!")
+
+		return users, nil
 	}
 
-	if err != nil {
-		fmt.Printf("an error occured looking up twitter profiles: %v error: %s response code: [%v]\n", userLookupParams.ScreenName, err, tres.StatusCode)
-	}
-
-	fmt.Println("Twitter lookup finished successfully!")
-	return users
+	return nil, errors.New("Twitter client object is uninitialized. it might be nil")
 }
 
 //LastTweetTime retrieves the given user id's last tweet time
