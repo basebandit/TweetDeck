7713cd21b59837a887c8c034ca21cbace3c836c5
basebandit <evansonmwangi83@gmail.com>
2020-12-01 16:34:47 +0300

    add title

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  chainWebpack: config => {
+    config
+        .plugin('html')
+        .tap(args => {
+            args[0].title = "Avatarlysis";
+            return args;
+        })
+},
   "transpileDependencies": [
     "vuetify"
   ]
