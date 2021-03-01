50cc98219266c97b0f4ed9fa5a4188d447129150
basebandit <evansonmwangi83@gmail.com>
2021-03-01 11:11:10 +0300

    edit code comment

--- business/data/avatar/avatar.go
+++ business/data/avatar/avatar.go
@@ -887,7 +887,7 @@ func intPointer(i int) *int {
 	return &i
 }
 
-// intPointer is a helper to get a *int from a int. It is in the tests package
+// stringPointer is a helper to get a *string from a string. It is in the tests package
 // because we normally don't want to deal with pointers to basic types but it's
 // useful in some tests.
 func stringPointer(s string) *string {
